ea091f8ecfaae4c8749a0caa4c83a3c1d7e2fe8c
rajakushwah <raja.kushwah@zs.com>
2021-08-26 16:06:20 +0530

    settings apps

--- smartapp/settings.py
+++ smartapp/settings.py
@@ -126,7 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #add your static files in this directory
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/app'),
 )
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
