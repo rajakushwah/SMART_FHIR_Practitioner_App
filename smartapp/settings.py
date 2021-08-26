0b94dd6a2ea311a0e322f4d9890ebb41051ce78b
rajakushwah <raja.kushwah@zs.com>
2021-08-26 15:45:27 +0530

    Update settings.py

--- smartapp/settings.py
+++ smartapp/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/app'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
