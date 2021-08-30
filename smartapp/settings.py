608e798dd4c46dc00eda851f02d9944c7c1a756b
rajakushwah <raja.kushwah@zs.com>
2021-08-30 21:08:31 +0530

    Static path changed

--- smartapp/settings.py
+++ smartapp/settings.py
@@ -119,14 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'app/static/'
+STATIC_URL = '/static/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #add your static files in this directory
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'app/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
