1da18ac4e9d67d6e581a42db431d36e34c021890
rajakushwah <raja.kushwah@zs.com>
2021-08-30 19:00:37 +0530

    Settings

--- smartapp/urls.py
+++ smartapp/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.conf.urls.static import static
-from django.conf import settings
+# from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +25,8 @@ urlpatterns = [
 ]
 
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# urlpatterns = i18n_patterns(path('admin/', admin.site.urls), prefix_default_language=False) + \
+#     static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
+#     static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
